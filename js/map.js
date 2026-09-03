3e81b30f1b1873a3dd663a95c998f6733c315e01
Jotaro-Caguamas <epic.joni.2007@gmail.com>
2026-09-03 15:13:19 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,9 +1,51 @@
+// ========================================
+// PAREDES
+// ========================================
+
+export const walls = [
+
+    // Pared superior
+    {
+        x: 100,
+        y: 100,
+        width: 800,
+        height: 40
+    },
+
+    // Pared izquierda
+    {
+        x: 100,
+        y: 100,
+        width: 40,
+        height: 500
+    },
+
+    // Pared derecha
+    {
+        x: 860,
+        y: 100,
+        width: 40,
+        height: 500
+    },
+
+    // Pared inferior
+    {
+        x: 100,
+        y: 560,
+        width: 800,
+        height: 40
+    }
+
+];
+
+
+// ========================================
+// DIBUJAR MAPA
+// ========================================
+
 export function drawMap(ctx, width, height) {
 
-    // ==========================
-    // FONDO DEL BACKROOMS
-    // ==========================
-
+    // Fondo
     ctx.fillStyle = "#c9bd6b";
 
     ctx.fillRect(
@@ -12,5 +54,20 @@ export function drawMap(ctx, width, height) {
         width,
         height
     );
+
+
+    // Paredes
+    ctx.fillStyle = "#7b3fb5";
+
+    for (const wall of walls) {
+
+        ctx.fillRect(
+            wall.x,
+            wall.y,
+            wall.width,
+            wall.height
+        );
+
+    }
 
 }
