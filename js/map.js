3b71d6e96aba0697d5483e08ef00f09d7a4518aa
Jotaro-Caguamas <epic.joni.2007@gmail.com>
2026-09-03 14:46:55 -0500

    Create map.js

---
+++ js/map.js
@@ -0,0 +1,16 @@
+export function drawMap(ctx, width, height) {
+
+    // ==========================
+    // FONDO DEL BACKROOMS
+    // ==========================
+
+    ctx.fillStyle = "#c9bd6b";
+
+    ctx.fillRect(
+        0,
+        0,
+        width,
+        height
+    );
+
+}
