f7289fe15ac206ab57cd4a76edfe0de25ca85b29
Jotaro-Caguamas <epic.joni.2007@gmail.com>
2026-09-03 14:45:19 -0500

    Update juego.js

--- js/juego.js
+++ js/juego.js
@@ -1,9 +1,26 @@
+import { Player } from "./player.js";
+import { drawMap } from "./map.js";
+
+
+// ==========================
+// CANVAS
+// ==========================
+
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+ctx.imageSmoothingEnabled = false;
+
+
+// ==========================
+// TAMAÑO DE PANTALLA
+// ==========================
+
 function resizeCanvas() {
+
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
 }
 
 resizeCanvas();
@@ -11,67 +28,83 @@ resizeCanvas();
 window.addEventListener("resize", resizeCanvas);
 
 
-// Jugador temporal
-let player = {
-    x: canvas.width / 2,
-    y: canvas.height / 2,
-    size: 40,
-    speed: 4
-};
+// ==========================
+// PLAYER
+// ==========================
+
+const player = new Player(
+    canvas.width / 2,
+    canvas.height / 2
+);
 
 
-// Teclas
+// ==========================
+// TECLADO
+// ==========================
+
 const keys = {};
 
-window.addEventListener("keydown", (e) => {
-    keys[e.key.toLowerCase()] = true;
+window.addEventListener("keydown", (event) => {
+
+    keys[event.key.toLowerCase()] = true;
+
 });
 
-window.addEventListener("keyup", (e) => {
-    keys[e.key.toLowerCase()] = false;
+window.addEventListener("keyup", (event) => {
+
+    keys[event.key.toLowerCase()] = false;
+
 });
 
 
-// Actualizar jugador
+// ==========================
+// ACTUALIZAR JUEGO
+// ==========================
+
 function update() {
 
-    if (keys["w"]) player.y -= player.speed;
-    if (keys["s"]) player.y += player.speed;
-    if (keys["a"]) player.x -= player.speed;
-    if (keys["d"]) player.x += player.speed;
+    player.update(keys);
 
 }
 
 
-// Dibujar
+// ==========================
+// DIBUJAR JUEGO
+// ==========================
+
 function draw() {
 
-    // Fondo
-    ctx.fillStyle = "#c9bd6b";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-
-    // Jugador temporal
-    ctx.fillStyle = "red";
-
-    ctx.fillRect(
-        player.x - player.size / 2,
-        player.y - player.size / 2,
-        player.size,
-        player.size
+    // Mapa
+    drawMap(
+        ctx,
+        canvas.width,
+        canvas.height
     );
+
+
+    // Player
+    player.draw(ctx);
 
 }
 
 
-// Bucle
+// ==========================
+// GAME LOOP
+// ==========================
+
 function gameLoop() {
 
     update();
+
     draw();
 
     requestAnimationFrame(gameLoop);
 
 }
+
+
+// ==========================
+// INICIAR
+// ==========================
 
 gameLoop();
